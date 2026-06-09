acc4b916ff0fe943b08a886935b3a4a09271338b
bekaretzkaa <bekaretzka@gmail.com>
2026-06-09 16:54:01 +0500

    Bottom Navigation: updated

--- app/src/main/java/com/example/boredomfocus/ui/MainActivity.kt
+++ app/src/main/java/com/example/boredomfocus/ui/MainActivity.kt
@@ -6,34 +6,84 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.NavHostFragment
+import com.example.boredomfocus.R
 import com.example.boredomfocus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            val customPadding = (24 * resources.displayMetrics.density).toInt()
             v.setPadding(
-                systemBars.left + customPadding,
-                systemBars.top + customPadding,
-                systemBars.right + customPadding,
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
                 systemBars.bottom
             )
             insets
         }
-//        startActivity(Intent(this, StopwatchActivity::class.java))
-//        startActivity(Intent(this, DetoxTimerActivity::class.java))
-        startActivity(Intent(this, OnboardingActivity::class.java))
 
-        binding.btnStartDetox.setOnClickListener {
-            startActivity(Intent(this, FocusResultActivity::class.java))
+        val navHost = supportFragmentManager.findFragmentById(binding.mainFragmentContainer.id) as NavHostFragment
+        navController = navHost.navController
+
+        setupBottomNavigation()
+        observeDestinations()
+    }
+
+    private fun observeDestinations() {
+        navController.addOnDestinationChangedListener {
+                _, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment -> showBottomNav()
+                R.id.statisticsFragment -> showBottomNav()
+                R.id.settingsFragment -> showBottomNav()
+                else -> hideBottomNav()
+            }
         }
+    }
+
+    private fun setupBottomNavigation() {
+        binding.llNavHome.setOnClickListener {
+            navigateToTopLevel(R.id.homeFragment)
+        }
+
+        binding.llNavStatistics.setOnClickListener {
+            navigateToTopLevel(R.id.statisticsFragment)
+        }
+
+        binding.llNavSettings.setOnClickListener {
+            navigateToTopLevel(R.id.settingsFragment)
+        }
+    }
+
+    private fun navigateToTopLevel(destinationId: Int) {
+        val navOptions = NavOptions.Builder()
+            .setLaunchSingleTop(true)
+            .setRestoreState(true)
+            .setPopUpTo(navController.graph.startDestinationId, inclusive = false, saveState = true)
+            .build()
+        
+        navController.navigate(destinationId, null, navOptions)
+    }
+
+    private fun showBottomNav() {
+        binding.bottomNavContainer.isVisible = true
+    }
+
+    private fun hideBottomNav() {
+        binding.bottomNavContainer.isVisible = false
     }
 }
---
+++ app/src/main/java/com/example/boredomfocus/ui/StatisticsFragment.kt
@@ -0,0 +1,8 @@
+package com.example.boredomfocus.ui
+
+import androidx.fragment.app.Fragment
+import com.example.boredomfocus.R
+
+class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
+
+}
