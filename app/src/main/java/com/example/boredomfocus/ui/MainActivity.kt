d208e7ac65db404c32d86ef7f12c0e20e33c0869
bekaretzkaa <bekaretzka@gmail.com>
2026-06-09 12:07:52 +0500

    Stopwatch logic and UI

--- app/src/main/java/com/example/boredomfocus/ui/MainActivity.kt
+++ app/src/main/java/com/example/boredomfocus/ui/MainActivity.kt
@@ -28,7 +28,8 @@ class MainActivity : AppCompatActivity() {
             )
             insets
         }
-        startActivity(Intent(this, DetoxTimerActivity::class.java))
+        startActivity(Intent(this, StopwatchActivity::class.java))
+//        startActivity(Intent(this, DetoxTimerActivity::class.java))
 //        startActivity(Intent(this, OnboardingActivity::class.java))
     }
 }
